53b8b44d98579b587c056a364316ef744fd075e8
IndraRadek <zoltko.radek@gmail.com>
2021-01-23 12:47:14 +0100

    Add new files

---
+++ apierwszyprogram/Main03.java
@@ -0,0 +1,13 @@
+package pl.coderslab.apierwszyprogram;
+
+public class Main03 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Dzisiaj zaczynam kurs");
+		System.out.println("JAVA");
+		System.out.println("w CodersLab");
+
+	}
+
+}
